2bae569297b92b27015853a04a9275cc14222fb8
JudgeLevia <ponycandy666@126.com>
2023-12-15 14:36:13 +0800

    some changes

--- gpcsnode/include/gpcsmat.h
+++ gpcsnode/include/gpcsmat.h
@@ -6,18 +6,41 @@ namespace gpcs
     {
         int rows;
         int cols;
-        std::vector<std::vector<double>> data;
+        std::vector<double> data;
         template<class Archive>
         void serialize(Archive& ar, const unsigned int version) {
             ar& rows;
             ar& cols;
             ar& data;
         }
-        std::vector<double>& operator[] (int i) {
-            return data[i];
+        double* operator[] (int i) {
+            // check if the index is valid
+            if (i < 0 || i >= rows)
+            {
+                throw std::out_of_range("Invalid row index");
+            }
+            // return the pointer to the row
+            return data.data() + i * cols;
         }
-        const std::vector<double>& operator[] (int i) const {
-            return data[i];
+        const double* operator[] (int i) const {
+            // check if the index is valid
+            if (i < 0 || i >= rows) {
+                throw std::out_of_range("Invalid row index");
+            }
+            // return the pointer to the row
+            return data.data() + i * cols;
+        }
+        void resize(int nrows,int ncols)
+        {
+            rows = nrows;
+            cols = ncols;
+            int totalsize = rows * cols;
+            data.reserve(totalsize);
+            for (int i = 1; i <= totalsize; i++)
+            {
+                double v=0;
+                data.push_back(v);
+            }
         }
     };
 }
